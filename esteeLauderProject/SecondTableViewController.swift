f2d712752566f349e1f86c6778b5ca9547ce52ba
julie kim <dahyunjulie@gmail.com>
2022-06-16 15:26:37 -0500

    draft1

---
+++ esteeLauderProject/SecondTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondTableViewController.swift
+//  esteeLauderProject
+//
+//  Created by Julie Kim on 6/15/22.
+//
+
+import UIKit
+
+class SecondTableViewController: UITableViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        }
+
+    
+    }
+    
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+
+        
+    
+    
+    
+
